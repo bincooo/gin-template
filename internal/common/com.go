27d96e965fc02817be9da5b2400384b2c071004a
bincooo <bingco.zn@gmail.com>
2024-06-23 08:11:06 +0800

    feat: add common.AddExited

--- internal/common/com.go
+++ internal/common/com.go
@@ -1,13 +1,34 @@
 package common
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 var initFunctions = make([]func(), 0)
+var exitFunctions = make([]func(), 0)
 
 func InitCommon() {
 	for _, apply := range initFunctions {
 		apply()
 	}
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	go func(ch chan os.Signal) {
+		<-ch
+		for _, apply := range exitFunctions {
+			apply()
+		}
+		os.Exit(0)
+	}(ch)
 }
 
 func AddInitialized(apply func()) {
 	initFunctions = append(initFunctions, apply)
+}
+
+func AddExited(apply func()) {
+	exitFunctions = append(exitFunctions, apply)
 }
